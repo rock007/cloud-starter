91cec498c450de991d9d6555f04878aa2392140f
sam <rock0715@outlook.com>
2017-06-08 15:46:05 +0800

    init-not-ready

---
+++ cloud-discovery/src/main/java/org/cloud/discovery/Application.java
@@ -0,0 +1,18 @@
+package org.cloud.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * discovery server
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class Application 
+{   
+    public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
